ac70a5846cb3fe46f535b4f1e437ccf2af222dd3
prushni1 <jprushni@gmail.com>
2016-08-29 11:57:50 +0530

    Add files via upload

---
+++ PrintNameWithoutSemiColon.java
@@ -0,0 +1,17 @@
+
+
+
+/**
+ * Write a program to print your name without using semicolon in the program.
+ */
+public class PrintNameWithoutSemiColon {
+	
+	 public static void main(String[] args) {
+		 
+	        if (System.out.printf("PRUSHNI \n") == null) {}
+
+	      
+	        if (System.out.append(" JANI\n").equals(null)) {}
+	    }
+
+}
